5dd6ffe4b18823cad75eafa5fb677ee50e894736
alcl2000 <amy.555.lewis@gmail.com>
2023-02-15 14:17:20 +0000

    fix: django version downgraded to fix csrf issue

--- forum/settings.py
+++ forum/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'forum',
 ]
 
 MIDDLEWARE = [
