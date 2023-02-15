40fa5cf5ee476f00b6273195a2ae6a4b1f15ad7f
alcl2000 <amy.555.lewis@gmail.com>
2023-02-15 17:07:28 +0000

    add: category model

--- citizen_detectives/models.py
+++ citizen_detectives/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.model):
+    category_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=25, unique=True)
+    description = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        ordering = ['alphabet']
+    
+    def category_name(self):
+        return self.title
+
+    def category_desc(self):
+        return self.description
