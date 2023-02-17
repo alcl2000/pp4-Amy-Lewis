0c43f8699131129f5fc350466b76aee76fcf8c3c
alcl2000 <amy.555.lewis@gmail.com>
2023-02-17 13:14:13 +0000

    add: ability to create tags

--- citizen_detectives/admin.py
+++ citizen_detectives/admin.py
@@ -6,7 +6,7 @@ from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 @admin.register(Category)
 class CategoryAdmin(SummernoteModelAdmin):
-    list_display = ('title',)
+    list_display = ('title', 'description')
 
 
 @admin.register(Tag)
--- citizen_detectives/models.py
+++ citizen_detectives/models.py
@@ -16,12 +16,24 @@ class Category(models.Model):
 
     def category_desc(self):
         return self.description
+    
+    @classmethod
+    def get_default_pk(cls):
+        exam, created = cls.objects.get_or_create(
+            title='Default Category', 
+            defaults=dict(description='This is a test Category'),
+        )
+        return exam.pk
 
 
 class Tag(models.Model):
+    tag_catagory = models.ForeignKey('Category', 
+                                     to_field='category_id',
+                                     default=Category.get_default_pk,
+                                     on_delete=models.CASCADE)
     tag_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=30, unique=True)
-    
+
     class ColourOptions(models.TextChoices):
         # options for the tag colours 
         RED = 'RED', ('Red')
@@ -40,3 +52,9 @@ class Tag(models.Model):
     
     def tag(self):
         return self.title, self.tag_colour
+
+
+# class Post(models.Model):
+#     post_id = models.AutoField(primary_key=True)
+#     post_category = models.ForeignKey('Category', on_delete=models.CASCADE)
+#     post_tag = models.ForeignKey('Tag', on_delete=models.)
