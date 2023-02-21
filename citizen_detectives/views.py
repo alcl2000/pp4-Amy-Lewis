e21179f172153611f6fbe860ed60fc4c8feb8d96
alcl2000 <amy.555.lewis@gmail.com>
2023-02-21 11:51:20 +0000

    add: categorylist view

--- citizen_detectives/views.py
+++ citizen_detectives/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from .models import Category
+
 
 # Create your views here.
+class CategoryList(View):
+    model = Post
+    queryset = Post.objects.order_by('-category_id')
+    template_name = "category-list.html"
