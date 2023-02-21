ce27c3a04c1428f33eb0ede4c99e470c41236d53
alcl2000 <amy.555.lewis@gmail.com>
2023-02-21 14:06:32 +0000

    add: url for the category view

--- citizen_detectives/views.py
+++ citizen_detectives/views.py
@@ -5,6 +5,6 @@ from .models import Category
 
 # Create your views here.
 class CategoryList(View):
-    model = Post
-    queryset = Post.objects.order_by('-category_id')
+    model = Category
+    queryset = Category.objects.order_by('-category_id')
     template_name = "category-list.html"
