b46c3d5485d59e8918dc31180251b064781e7e08
alcl2000 <amy.555.lewis@gmail.com>
2023-02-24 17:52:00 +0000

    add: add category form created

--- forum/views.py
+++ forum/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from citizen_detectives.models import Category
+from citizen_detectives.forms import CategoryForm
 
 
 # Create your views here.
@@ -8,3 +9,4 @@ class CategoryList(generic.ListView):
     model = Category
     queryset = Category.objects.order_by('-category_id')
     template_name = "category_list.html"
+    # post form to create new categories
