0a70505f4e8753f6a2399e62f612b27f847108ac
alcl2000 <amy.555.lewis@gmail.com>
2023-02-27 17:28:55 +0000

    add: form and form data added

--- forum/views.py
+++ forum/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from django.views import generic, View
 from citizen_detectives.models import Category
 from citizen_detectives.forms import CategoryForm
@@ -10,3 +10,9 @@ class CategoryList(generic.ListView):
     queryset = Category.objects.order_by('-category_id')
     template_name = "category_list.html"
     # post form to create new categories
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryList, self).get_context_data(**kwargs)
+        context['form'] = CategoryForm()
+        return context
+        return HttpResponseRedirect('/categories/')
