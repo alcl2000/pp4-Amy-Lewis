e7d78b12aaa1b722aa8fb4a3dbe404751cf13d68
alcl2000 <amy.555.lewis@gmail.com>
2023-02-28 15:16:11 +0000

    add: get add category test passing

--- citizen_detectives/test_views.py
+++ citizen_detectives/test_views.py
@@ -7,3 +7,8 @@ class TestViews(TestCase):
         response = self.client.get('/categories/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed('category_list.html')
+    
+    def test_get_add_category_page(self):
+        response = self.client.get('/add-category/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('add_category.html')
