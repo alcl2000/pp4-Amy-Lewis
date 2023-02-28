24454e1ebc387bd5405cc9e5e09b1e631f6a67fe
alcl2000 <amy.555.lewis@gmail.com>
2023-02-28 12:12:53 +0000

    fix: category add form submitting correctly

--- forum/urls.py
+++ forum/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('categories/', views.CategoryList.as_view(), name='category_view')
+    path('categories/', views.CategoryList.as_view(), name='category_view'),
+    path('add-category/', views.add_category, name='add_category')
 ]
