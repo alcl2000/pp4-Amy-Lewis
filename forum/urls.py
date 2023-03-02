627fb5adcfd5209c45f14b3619fda33d12cbd46c
alcl2000 <amy.555.lewis@gmail.com>
2023-03-02 17:57:50 +0000

    fix: url refactored

--- forum/urls.py
+++ forum/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('categories/', views.CategoryList.as_view(), name='category_view'),
     path('add-category/', views.add_category, name='add_category'),
-    path('category/<slug:id>', views.CategoryDetailView.as_view(), name='category_detail')
+    path('category/<slug:slug>', 
+         views.CategoryDetailView.as_view(), 
+         name='category_detail')
 ]
