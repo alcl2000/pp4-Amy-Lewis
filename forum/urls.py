9b3a42023ad3f6cf0861767e4f3eedac0159d826
alcl2000 <amy.555.lewis@gmail.com>
2023-03-02 12:20:50 +0000

    add: url for category_detail view

--- forum/urls.py
+++ forum/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('categories/', views.CategoryList.as_view(), name='category_view'),
-    path('add-category/', views.add_category, name='add_category')
+    path('add-category/', views.add_category, name='add_category'),
+    path('category/<slug:id>', views.CategoryDetailView.as_view(), name='category_detail')
 ]
