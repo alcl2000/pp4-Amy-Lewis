592f50024eccdb0bd45305f9f1142f514099f5f2
alcl2000 <amy.555.lewis@gmail.com>
2023-03-02 17:44:01 +0000

    add: category form now generates a slug for urls

--- citizen_detectives/admin.py
+++ citizen_detectives/admin.py
@@ -6,16 +6,16 @@ from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 @admin.register(Category)
 class CategoryAdmin(SummernoteModelAdmin):
-    list_display = ('title', 'description')
+    list_display = ('title', 'description','slug')
 
 
 @admin.register(Tag)
-class CategoryAdmin(SummernoteModelAdmin):
+class TagAdmin(SummernoteModelAdmin):
     list_display = ('title', 'tag_colour', 'tag_category_name')
     list_filter = ['tag_category__title', 'tag_colour']
 
 
 @admin.register(Post)
-class CategoryAdmin(admin.ModelAdmin):
+class PostAdmin(admin.ModelAdmin):
     list_display = ('post_title', 'post_author', 'post_category', 'post_tag')
     list_filter = ['post_category', 'post_tag']
