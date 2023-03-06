9f5c7e95862077434556323114ec3b2664b36af9
alcl2000 <amy.555.lewis@gmail.com>
2023-03-06 11:47:43 +0000

    add: post admin model

--- citizen_detectives/admin.py
+++ citizen_detectives/admin.py
@@ -6,10 +6,17 @@ from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 @admin.register(Category)
 class CategoryAdmin(SummernoteModelAdmin):
-    list_display = ('title', 'description')
+    list_display = ('title', 'description', 'slug')
 
 
 @admin.register(Tag)
 class CategoryAdmin(SummernoteModelAdmin):
     list_display = ('title', 'tag_colour', 'tag_category_name')
     list_filter = ['tag_category__title', 'tag_colour']
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('post_title', 'post_author', 'post_category', 'post_tag')
+    list_filter = ['post_category', 'post_tag']
+    prepopulated_fields = {'slug': ('post_title',)}
