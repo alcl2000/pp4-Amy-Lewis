9c1a57a406249b07a52c5de965e49bb3f078f885
alcl2000 <amy.555.lewis@gmail.com>
2023-03-08 15:21:29 +0000

    add: tag form file

--- citizen_detectives/forms.py
+++ citizen_detectives/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Category
+from .models import Category, Tag
 
 
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = ['title', 'description']
+
+
+class TagForm(forms.ModelFrom):
+    class Meta:
+        model = Tag
+        fields = ['title', 'tag_colour']
