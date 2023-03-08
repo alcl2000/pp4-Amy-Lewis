9eca41ed20f0ed437b752faa6aa4051241bdb693
alcl2000 <amy.555.lewis@gmail.com>
2023-03-08 14:29:49 +0000

    add: edit category link/url added

--- forum/urls.py
+++ forum/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('category/<slug:slug>', 
          views.CategoryDetailView.as_view(), 
          name='category_detail'),
-    path('delete/<category_id>', views.delete_category, name='delete_category')
+    path('delete/<category_id>', 
+         views.delete_category, 
+         name='delete_category'),
+    path('edit/<category_id>', views.edit_category, name='edit_category')
 ]
