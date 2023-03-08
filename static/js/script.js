1e636425f344e254b93b7ac904977a954391ec01
alcl2000 <amy.555.lewis@gmail.com>
2023-03-08 12:39:53 +0000

    fix: confirmation message edited

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     $(".delete").click(function(){
-        return confirm('Are you sure you want to delete?')
+        return confirm('Are you sure you want to delete this category? All posts in this category will be deleted. This action cannot be undone')
     })
 })
