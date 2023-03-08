f4be202899959a333d7f1cc6a46496cd99613c13
alcl2000 <amy.555.lewis@gmail.com>
2023-03-08 12:44:18 +0000

    add: sweet alerts added

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
+const Swal = require('sweetalert2')
 $(document).ready(function(){
     $(".delete").click(function(){
         return confirm('Are you sure you want to delete this category? All posts in this category will be deleted. This action cannot be undone')
