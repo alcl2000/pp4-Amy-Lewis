5c0dac59fa4fb5f9335c3a2461ef16b134edb2dc
alcl2000 <amy.555.lewis@gmail.com>
2023-03-16 10:18:52 +0000

    fix: form validation fixed

--- forum/views.py
+++ forum/views.py
@@ -43,11 +43,10 @@ def delete_category(request, category_id):
 def edit_category(request, category_id):
     category = get_object_or_404(Category, category_id=category_id)
     if request.method == 'POST':
-        if request.method == 'POST':
-            form = CategoryForm(request.POST, instance=category)
-            if form.is_valid():
-                form.save()
-                return redirect('/categories')
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('/categories')
     form = CategoryForm(instance=category)
     title_and_text = "Edit Category"
     context = {
