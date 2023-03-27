9c648a80936c197cbdf62f54b3d1f6a4929267cb
alcl2000 <amy.555.lewis@gmail.com>
2023-03-27 19:07:49 +0000

    fix: removed swal

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,3 @@
-const Swal = require('sweetalert2')
 $(document).ready(function(){
     $(".delete").click(function(){
         return confirm('Are you sure you want to delete this category? All posts in this category will be deleted. This action cannot be undone')
